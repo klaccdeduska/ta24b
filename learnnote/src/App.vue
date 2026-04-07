f83da65c5a9cf1b45a034393d89536c0447d6eae
Mihhail Burov <burov1808@gmail.com>
2026-04-07 23:30:29 +0300

    Add reverse text example

--- learnnote/src/App.vue
+++ learnnote/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { ref } from 'vue';
+
+let message = ref("Hello vue!");
 
 </script>
 
 <template>
-
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <button class="button is-primary" @click="message = 'Hello TA24B'">Click me!</button>
+    <input class="input" v-model="message">
 </template>
 
 <style>
