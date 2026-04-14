7ace5e9924ba55c86e18eebc6d23c98db6518fc5
Mihhail Burov <burov1808@gmail.com>
2026-04-14 19:29:53 +0300

    Add itemlist component

--- learnnote/src/App.vue
+++ learnnote/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed, ref } from 'vue';
+import ItemList from './ItemList.vue';
 let i = 1;
 
 let items = ref([
@@ -34,30 +35,9 @@ function add(){
             </div>
         </div>
         <div class="content">
-        <h1>All items</h1>
-        <ul>
-          <li v-for="item in items" :key="item.id">
-            {{ item.text }}
-            <input type="checkbox" v-model="item.isDone">
-          </li>
-        </ul>
-
-        <h1>Done items</h1>
-        <ul>
-          <li v-for="item in doneItems" :key="item.id">
-            {{ item.text }}
-            <input type="checkbox" v-model="item.isDone">
-          </li>
-        </ul>
-
-        <h1>ToDo items</h1>
-        <ul>
-          <li v-for="item in ToDoItems" :key="item.id">
-            {{ item.text }}
-            <input type="checkbox" v-model="item.isDone">
-          </li>
-        </ul>
-
+            <ItemList :items="items" title="All items"></ItemList>
+            <ItemList :items="doneItems" title="Done items"></ItemList>
+            <ItemList :items="ToDoItems" title="ToDo items"></ItemList>
       </div>
     </div>
 </template>
