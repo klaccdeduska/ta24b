6150350d0bf137cabcfa3d530583b7fc719d7c56
Mihhail Burov <burov1808@gmail.com>
2026-04-18 18:34:19 +0300

    Add vue router

--- learnnote/src/index.js
+++ learnnote/src/index.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue';
+import { createMemoryHistory, createWebHashHistory, createWebHistory, createRouter } from 'vue-router';
+
+import ToDo from './pages/ToDo.vue';
+import Modals from './pages/Modals.vue';
+
+const routes = [
+  { path: '/', component: ToDo },
+  { path: '/modals', component: Modals },
+];
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+
+
 // import the root component App from a single-file component.
 import App from './App.vue';
 
-const app = createApp(App).mount("#app");
+const app = createApp(App).use(router).mount("#app");
