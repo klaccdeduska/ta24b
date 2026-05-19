844adbd4d567c32495bef08e5c980bd459f5d7ec
Mihhail Burov <burov1808@gmail.com>
2026-05-19 23:39:08 +0300

    Add message

---
+++ learnnote/src/pages/Messages.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Message from '../components/Message.vue';
+
+
+</script>
+<template>
+    <Message color="info" large>
+        Hello <b>TA-24B</b>
+    </Message>
+</template>
